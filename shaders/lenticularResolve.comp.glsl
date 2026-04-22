ec289ed66f1ba2732554ddf5deb3d30044b05a5d
jb <jb239812@ohio.edu>
2026-04-22 16:34:06 -0400

    bump resolution, cleanup SDF shader stuff

--- shaders/lenticularResolve.comp.glsl
+++ shaders/lenticularResolve.comp.glsl
@@ -29,49 +29,6 @@ vec2 RaySphereIntersect ( vec3 r0, vec3 rd, vec3 s0, float sr ) {
 	}
 }
 
-float de( vec3 p ){
-	// spatial repeats
-	pMod3( p, vec3( 5.0f ) );
-
-	float s = 2.;
-	float e = 0.;
-	for(int j=0;++j<7;)
-	p.xz=abs(p.xz)-2.3,
-	p.z>p.x?p=p.zyx:p,
-	p.z=1.5-abs(p.z-1.3+sin(p.z)*.2),
-	p.y>p.x?p=p.yxz:p,
-	p.x=3.-abs(p.x-5.+sin(p.x*3.)*.2),
-	p.y>p.x?p=p.yxz:p,
-	p.y=.9-abs(p.y-.4),
-	e=12.*clamp(.3/min(dot(p,p),1.),.0,1.)+
-	2.*clamp(.1/min(dot(p,p),1.),.0,1.),
-	p=e*p-vec3(7,1,1),
-	s*=e;
-	return length(p)/s;
-}
-
-const float epsilon = 0.001f;
-bool didHit = false;
-float raymarch ( vec3 rayOrigin, vec3 rayDirection ) {
-	float dQuery = 0.0f;
-	float dTotal = 0.0f;
-	vec3 pQuery = rayOrigin;
-	for ( int steps = 0; steps < 200; steps++ ) {
-		pQuery = rayOrigin + dTotal * rayDirection;
-		dQuery = de( pQuery );
-		dTotal += dQuery * 0.9f;
-		didHit = ( abs( dQuery ) < epsilon );
-		if ( dTotal > 20.0f || didHit ) {
-			break;
-		}
-	}
-	return dTotal;
-}
-
-vec3 SDFNormal ( in vec3 position ) {
-	vec2 e = vec2( epsilon, 0.0f );
-	return normalize( vec3( de( position ) ) - vec3( de( position - e.xyy ), de( position - e.yxy ), de( position - e.yyx ) ) );
-}
 
 void main () {
 	ivec2 idx = ivec2( gl_GlobalInvocationID.xy );
@@ -80,8 +37,7 @@ void main () {
 	ivec2 pixelIdx = idx / GlobalData.gridDivisions;
 	vec3 color = vec3( 1.0f );
 
-#if 1
-
+	// this will now be doing a DDA traversal towards the heightmap, using the same origin, direction
 
 	vec3 sphereCenter = vec3( 0.0f, 0.0f, -1.1025f );
 	int numSamples = 256;
@@ -109,40 +65,6 @@ void main () {
 		}
 	}
 	color = c / float( numSamples );
-#else
-	// we need to figure out what pixel this invocation corresponds to -> informs ray origin
-	vec3 rayOrigin = vec3(
-		remap( float( pixelIdx.x + 0.5f ), 0.0f, GlobalData.gridBaseDim - 1, -1.0f, 1.0f ),
-		remap( float( pixelIdx.y + 0.5f ), 0.0f, GlobalData.gridBaseDim - 1, -1.0f, 1.0f ),
-		0.0f
-	);
-
-	// we need to figure out what angle this invocation corresponds to -> informs ray direction
-	ivec2 subpixelIdx = idx % GlobalData.gridDivisions;
-	vec2 startAngle = vec2( // this is used to construct the vector, via rotations from a vector pointing downwards...
-		remap( float( subpixelIdx.x ), 0.0f, GlobalData.gridDivisions - 1, piHalf * GlobalData.angleScale, -piHalf * GlobalData.angleScale ),
-		remap( float( subpixelIdx.y ), 0.0f, GlobalData.gridDivisions - 1, piHalf * GlobalData.angleScale, -piHalf * GlobalData.angleScale )
-	);
-	vec3 rayDirection = Rotate3D( startAngle.x, vec3( 0.0f, 1.0f, 0.0f ) ) * Rotate3D( startAngle.y, vec3( 1.0f, 0.0f, 0.0f ) ) * vec3( 0.0f, 0.0f, -1.0f );
-	float dRaymarch = raymarch( rayOrigin + vec3( 0.0f, 0.0f, 0.75f ), rayDirection );
-	if ( didHit ) {
-		const vec3 normal = SDFNormal( rayOrigin + dRaymarch * rayDirection );
-		const vec3 pHit = rayOrigin + dRaymarch * rayDirection + 3.0f * epsilon * normal;
-		const vec3 pLight = vec3( 0.0f );
-		const vec3 vLight = pLight - pHit;
-		const vec3 vLightNorm = normalize( vLight );
-
-		color = vec3( 0.75f, 0.5f, 0.3f );
-
-		// shadow trace
-		if ( raymarch( pHit, vLightNorm ) < length( vLight ) ) {
-			// we hit something...
-			color *= 0.1f;
-		} else {
-			color *= saturate( dot( vLightNorm, normal ) );
-		}
-	}
-#endif
 
 	// we need to store back the result, into the lenticular LUT
 	imageStore( lenticularLUT, idx, vec4( color, 1.0f ) );
