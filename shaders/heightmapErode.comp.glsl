dafad399dfe275271f42538dd805d2b49ca7a593
jb <jb239812@ohio.edu>
2026-04-22 23:04:26 -0400

    heightmap sim

--- shaders/heightmapErode.comp.glsl
+++ shaders/heightmapErode.comp.glsl
@@ -12,23 +12,19 @@ layout ( r32i, set = 0, binding = 2 ) uniform iimage2D heightmap;
 #include "hg_sdf.h"
 #include "random.h"
 
-// using this for normal vector estimation
-vec2 RaySphereIntersect ( vec3 r0, vec3 rd, vec3 s0, float sr ) {
-	// r0 is ray origin
-	// rd is ray direction
-	// s0 is sphere center
-	// sr is sphere radius
-	// return is the roots of the quadratic, includes negative hits
-	float a = dot( rd, rd );
-	vec3 s0_r0 = r0 - s0;
-	float b = 2.0f * dot( rd, s0_r0 );
-	float c = dot( s0_r0, s0_r0 ) - ( sr * sr );
-	float disc = b * b - 4.0f * a * c;
-	if ( disc < 0.0f ) {
-		return vec2( -1.0f, -1.0f );
-	} else {
-		return vec2( -b - sqrt( disc ), -b + sqrt( disc ) ) / ( 2.0f * a );
+vec3 getNormal ( vec2 loc ) {
+	// maybe do something with more local points?
+	// get position within pixel..
+		// do the same test as the point sprites, cheaper than ray-sphere
+
+	// analytic solution via pythagoras
+//	vec2 centered = fract( loc ) * 2.0f - vec2( 1.0f );
+	vec2 centered = fract( loc );
+	float radiusSquared = dot( centered, centered );
+	if ( radiusSquared > 1.0f ) { // not on the sphere
+		return normalize( vec3( normalize( centered ), 1.0f ) );
 	}
+	return vec3( centered, sqrt( 1.0f - radiusSquared ) );
 }
 
 struct particle {
@@ -41,6 +37,7 @@ struct particle {
 void main () {
 	ivec2 idx = ivec2( gl_GlobalInvocationID.xy );
 	seed = PushConstants.wangSeed + idx.x * 69420 + idx.y * 8675309;
+	const vec2 iS = imageSize( heightmap ).xy;
 
 	// basic initialization
 	particle p;
@@ -50,10 +47,43 @@ void main () {
 
 	// generate an initial position on the surface
 	p.position = vec2(
-		remap( NormalizedRandomFloat(), 0.0f, 1.0f, 0.0f, imageSize( heightmap ).x - 1 ),
-		remap( NormalizedRandomFloat(), 0.0f, 1.0f, 0.0f, imageSize( heightmap ).y - 1 )
+		remap( NormalizedRandomFloat(), 0.0f, 1.0f, 0.0f, iS.x - 1 ),
+		remap( NormalizedRandomFloat(), 0.0f, 1.0f, 0.0f, iS.y - 1 )
 	);
 
-	// do the particle transport, for some number of steps
+	// do the particle transport
+		// break on:
+			// max number of steps
+			// particle goes out of bounds
+			// simulation stop conditions:
+				// p.volume is less than or equal GlobalData.minVolume
 
+	int maxSteps = 500;
+	while ( p.volume > GlobalData.minVolume && ( maxSteps-- != 0 ) ) {
+		// cached position
+		vec2 initialPosition = p.position;
+		vec3 normal = getNormal( p.position );
+
+		// newton's second law to calculate acceleration
+		p.speed += GlobalData.timeStep * normal.xz / ( p.volume * GlobalData.density ); // F = MA, A = F/M
+		p.position += GlobalData.timeStep * p.speed; // update position based on new speed
+		p.speed *= ( 1.0f - GlobalData.timeStep * GlobalData.friction ); // friction factor to attenuate speed
+
+		if ( !all( greaterThanEqual( p.position, vec2( 0.0f ) ) ) ||
+			!all( lessThan( p.position, iS ) ) ) break;
+
+		// sediment capacity
+		ivec2 refPoint = ivec2( p.position );
+		float maxSediment = p.volume * length( p.speed ) * ( ( imageLoad( heightmap, ivec2( initialPosition.xy ) ).r - imageLoad( heightmap, ivec2( p.position.xy ) ).r ) / float( GlobalData.maxHeight ) );
+		maxSediment = max( maxSediment, 0.0f ); // don't want negative values here
+		float sedimentDifference = maxSediment - p.sedimentFraction;
+
+		// update sediment content, deposit on the heightmap
+		p.sedimentFraction += GlobalData.timeStep * GlobalData.depositionRate * sedimentDifference;
+		float oldValue = imageLoad( heightmap, ivec2( initialPosition.xy ) ).r / float( GlobalData.maxHeight );
+		imageAtomicAdd( heightmap, ivec2( initialPosition.xy ), int( -0.1f * ( GlobalData.timeStep * p.volume * GlobalData.depositionRate * sedimentDifference ) * float( GlobalData.maxHeight ) ) );
+
+		// evaporate the droplet
+		p.volume *= ( 1.0f - GlobalData.timeStep * GlobalData.evaporationRate );
+	}
 }
